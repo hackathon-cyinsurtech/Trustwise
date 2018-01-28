8262f1663fbe79ff217d6347188c6f3058fbc82f
Nicolas Tsagarides <nicexe@gmail.com>
2018-01-28 12:13:36 +0200

    auto-update premium and total payout

--- frontend/js/input_validation.js
+++ frontend/js/input_validation.js
@@ -35,4 +35,34 @@ $(document).ready(function () {
         }
     });
 
+    $('#startTime').daterangepicker({
+        "singleDatePicker": true,
+        "showDropdowns": true,
+        "timePicker": true,
+        "timePicker24Hour": true,
+        "timePickerSeconds": true,
+        "autoApply": true,
+        "linkedCalendars": false,
+        "showCustomRangeLabel": false,
+        "startDate": "01/22/2018",
+        "endDate": "01/28/2018",
+        "opens": "center"
+    }, function(start, end, label) {
+    console.log("New date range selected: ' + start.format('X') + ' (predefined range: ' + label + ')");
+    });
+
+    $("#premium").keyup(function() {
+        var prem = +($(this).val());
+        var tot = Math.round(100000*(prem*100)/93)/100000;
+
+        $("#total").val(tot);
+    });
+
+    $("#total").keyup(function() {
+        var tot = +($(this).val());
+        var prem = Math.round(100000*(tot*93)/100)/100000;
+
+        $("#premium").val(prem);
+    });
+
 });
